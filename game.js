2268af96c0e32f229243cd030a67d6e407bfe9bf
Maii0407 <izuddin0407@gmail.com>
2021-10-10 14:39:50 +0800

    added more if...else if conditions

--- game.js
+++ game.js
@@ -9,14 +9,14 @@ function computerPlay() {
     }
 }
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    if ((playerSelection === 'rock' && computerSelection === 'scissors')||(playerSelection === 'paper' && computerSelection === 'rock')||(playerSelection === 'scissors' && computerSelection === 'paper')) {
         return alert ('YOU WIN');
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper')||(playerSelection === 'paper' && computerSelection === 'scissors')||(playerSelection === 'scissors' && computerSelection === 'rock')) {
         return alert ('YOU LOSE T_T');
-    } else if (playerSelection === 'rock' && computerSelection === 'rock') {
+    } else if ((playerSelection === 'rock' && computerSelection === 'rock')||(playerSelection === 'paper' && computerSelection === 'paper')||(playerSelection === 'scissors' && computerSelection === 'scissors')) {
         return alert('DRAW');
     } else {
-        return alert('FUCK INVALID AGAIN');
+        return alert('LOL INVALID');
     }
 }
 const playerSelection = prompt('CHOOSE YOUR WEAPON').toLowerCase();
