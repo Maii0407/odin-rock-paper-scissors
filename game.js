ec175e6bfd214d96a19952ef903af2275a0f1ed6
Maii0407 <izuddin0407@gmail.com>
2021-10-13 13:11:35 +0800

    lol not done yet just started using recursive functions instead of looping the function

--- game.js
+++ game.js
@@ -14,19 +14,30 @@ function playRound() {
     if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
     (playerSelection === 'paper' && computerSelection === 'rock') ||
     (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        userScore++;
         return 'win';
     } else if ((playerSelection === 'rock' && computerSelection === 'paper') ||
     (playerSelection === 'paper' && computerSelection === 'scissors') ||
     (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        computerScore++;
         return 'lose';
     } else {
         return 'draw';
     }
 }
+let userScore = parseInt(0);
+let computerScore = parseInt(0);
 
 
-function game() {
-    for (let i = 0; i < 5; i++)
+function game(i) {
     console.log(playRound());
+    console.log('Player Score = ' +userScore);
+    console.log('Computer Score = ' +computerScore);
+    if (i <= 5) {
+        game();
+    } else {
+        alert('GAME OVER => Player ('+userScore+') vs Computer ('+computerScore+')');
+    }
+    i++;
 }
-console.log(game());
+console.log(game(0));
