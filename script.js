8d98fe3f4e3993e5780cfc72a9e2ad225ee497aa
Akmal Izuddin <izuddin0407@gmail.com>
2021-10-27 22:45:29 +0800

    made it so app shows the current results of the game

--- script.js
+++ script.js
@@ -11,11 +11,13 @@ function playRound(e) {
 
     if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
     (playerSelection === 'paper' && computerSelection === 'rock') ||
-    (playerSelection === 'scissors' && computerSelection === 'paper')) {        
+    (playerSelection === 'scissors' && computerSelection === 'paper')) { 
+        document.getElementById('playerScore').innerHTML = ++playerScore;      
         alert('win');
     } else if ((playerSelection === 'rock' && computerSelection === 'paper') ||
     (playerSelection === 'paper' && computerSelection === 'scissors') ||
-    (playerSelection === 'scissors' && computerSelection === 'rock')) {       
+    (playerSelection === 'scissors' && computerSelection === 'rock')) { 
+        document.getElementById('computerScore').innerHTML = ++computerScore;     
         alert('lose');
     } else {
         alert('draw');
@@ -31,5 +33,20 @@ function computerPlay() {
         return 'paper';
     } else {
         return 'rock';
+    }
+}
+
+//player and computer score
+let playerScore = parseInt(0);
+let computerScore = parseInt(0);
+
+//keep score and announce winner
+function keepScore () {
+    if (playerScore === 5){
+        alert('CONGRATULATIONS YOU WIN');
+    } else if (computerScore === 5){
+        alert('LOL U GOT BEAT BY A ROBOT');
+    } else {
+        alert('error');
     }
 }
